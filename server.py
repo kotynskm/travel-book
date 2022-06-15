cb507237264a4743a142e031ee593734a4632d39
Kailey <k.kotynski@gmail.com>
2022-06-15 09:12:48 -0700

    Added trip_details.html, added route to display users trips

--- server.py
+++ server.py
@@ -4,7 +4,9 @@ import os
 from pprint import pprint as pp
 from model import User, Trip, Activity, connect_to_db, db
 from datetime import datetime
+from random import choice
 
+TRIP_IMAGES = [] # populate with images, then use random to send an img to trip_details through /trip/ route
 YELP_API_KEY = os.environ['YELP_API_KEY']
 yelp_url = 'https://api.yelp.com/v3'
 
@@ -91,21 +93,28 @@ def user_page():
     
     return render_template('homepage.html',trips=trips)
 
-@app.route('/api/activities')
-def show_activities():
-    # makes a call to yelp API to display activites in that city
-    # I need to be able to pull which city it is referring to and pass it into the API call
-    # trip = Trip.get_by_id() ??
-    url = 'https://api.yelp.com/v3/businesses/search'
-    headers = {'Authorization': 'Bearer %s' % YELP_API_KEY}
-    location = 'Seattle, WA'
-    params = {'location': location, 'limit':10,'sort_by':'rating'}
+@app.route('/trip/<trip_id>')
+def show_trip(trip_id):
+    trip = Trip.get_by_id(trip_id)
 
-    res = requests.get(url,headers=headers,params=params)
-    data = res.json()
+    return render_template('trip_details.html',trip=trip)
 
-    # renders activity page
-    return render_template('activities.html', data=data)
+# @app.route('/api/activities')
+# def show_activities():
+#     # makes a call to yelp API to display activites in that city
+#     # I need to be able to pull which city it is referring to and pass it into the API call
+#     # trip = Trip.get_by_id() ??
+#     trip = request.args.get('seattle')
+#     url = 'https://api.yelp.com/v3/businesses/search'
+#     headers = {'Authorization': 'Bearer %s' % YELP_API_KEY}
+#     location = trip
+#     params = {'location': location, 'limit':10,'sort_by':'rating'}
+
+#     res = requests.get(url,headers=headers,params=params)
+#     data = res.json()
+
+#     # renders activity page
+#     return render_template('activities.html', data=data,trip=trip)
 
 if __name__ == '__main__':
     connect_to_db(app)
